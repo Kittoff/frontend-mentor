e9ab5d2aeefc3747f9550e27f12c72f30e8524a0
Kittoff <j'aimegithub>
2022-07-29 21:11:58 +0200

    added container WIP structure + scss

---
+++ src/components/container/Container.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './container.css'
+
+
+const Container = (props: any) => {
+  return (
+    <div className='container'>{props.children}</div>
+  )
+}
+
+export default Container
